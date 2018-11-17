616f861300610ea6ce425de748db32f6f23d1e4e
Lukasz.Solarski <lukasz.solar@gmail.com>
2018-11-17 21:16:32 +0100

    Change the PropertyInjectedController: now it doesn't use the specific implementation, but the interface GreetingService.
    Changing the name of property to "greetingServiceImpl" tells the Spring that this controller should use the GreetingServiceImpl implementation (Bean). But in fact it's good habit to annotate it by @Qualifier to avoid further problems.

--- src/main/java/com/solar/guru/didemo/controller/PropertyInjectedController.java
+++ src/main/java/com/solar/guru/didemo/controller/PropertyInjectedController.java
@@ -1,16 +1,18 @@
 package com.solar.guru.didemo.controller;
 
-import com.solar.guru.didemo.service.GreetingServiceImpl;
+import com.solar.guru.didemo.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl;
 
     public String sayHello() {
-        return this.greetingService.sayHello();
+        return this.greetingServiceImpl.sayHello();
     }
 }
