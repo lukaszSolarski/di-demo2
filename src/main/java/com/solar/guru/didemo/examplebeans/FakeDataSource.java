425801ee7ff5026f1f5cc57e3c231d336f013de1
Lukasz.Solarski <lukasz.solar@gmail.com>
2018-12-28 16:24:33 +0100

    Refactor (create abstract class)

--- src/main/java/com/solar/guru/didemo/examplebeans/FakeDataSource.java
+++ src/main/java/com/solar/guru/didemo/examplebeans/FakeDataSource.java
@@ -1,22 +1,5 @@
 package com.solar.guru.didemo.examplebeans;
 
-public class FakeDataSource {
-    private String username;
-    private String password;
+public class FakeDataSource extends BaseFakeDataSource {
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
